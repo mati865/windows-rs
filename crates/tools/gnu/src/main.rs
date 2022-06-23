63e78b65581c2872fda5d3e30731a123528e0290
Mateusz Mikuła <mati865@gmail.com>
2022-06-23 22:33:07 +0200

    tmp

--- crates/tools/gnu/src/main.rs
+++ crates/tools/gnu/src/main.rs
@@ -135,7 +135,8 @@ fn build_mri(output: &std::path::Path, libraries: &BTreeMap<String, BTreeMap<Str
 
     mri.write_all(b"SAVE\nEND\n").unwrap();
 
-    let mut cmd = std::process::Command::new("ar");
+    let program = if platform == "aarch64_gnu" { "llvm-ar" } else { "ar" };
+    let mut cmd = std::process::Command::new(program);
     cmd.current_dir(&output);
     cmd.arg("-M");
     cmd.stdin(std::fs::File::open(&mri_path).unwrap());
